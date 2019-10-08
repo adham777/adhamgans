7cd3fe597c2abb2a6efb31079d735c667fdc50fb
adham777 <56234736+adham777@users.noreply.github.com>
2019-10-08 23:42:30 +0700

    Boostergame

---
+++ bostergame.py
@@ -0,0 +1,38 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t«----------✧----------»')
+print(a+'\t BOSTER ALL GAME')
+print(b+'\t «----------✧----------»')
+print(b+'\t ADHAM GANS')
+print(b+'\t  «----------✧----------»')
+print('\t https://github.com/adham777')
+print(a+'+'*40)
+print('\nProses..')
+sleep(3)
+print(b+'\n[!] menganalisis game..')
+sleep(3)
+try:
+      os.mkdir('/data/data/com.game/files/home/.game')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(3)
+print(b+'\n[!] Membersihkan sampah game...')
+sleep(3)
+
+print(a+'[!] Success !')
+sleep(1)
+print(b+'\n[!] Setting up..')
+sleep(2)
+os.system('game-reload-settings')
+print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi https://github.com/adham777 untuk requests\natau pertanyaan, atau hubungi https://t.me/CRABS_ID\nThanks :*\n\n')
+
+
+# ini cuma shortcut buat bantu para nub
+# adham L
